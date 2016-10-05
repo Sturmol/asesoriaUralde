17ee56270600db415a033f440a52d7ac26d4b287
Sturmol <sturmol@hotmail.com>
2016-10-05 17:52:30 +0200

    Gulp setup properly added

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ sass = require('gulp-sass'),
 browserSync = require('browser-sync');
 
 gulp.task('styles', function() {
-  gulp.src('./src/scss/*.scss')
+  gulp.src('./scss/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -16,24 +16,9 @@ gulp.task('serve', function() {
     }
   });
 
-  gulp.watch('./src/scss/*.scss', ['styles']);
-  gulp.watch('./src/**/*.html').on('change', browserSync.reload);
-});
-
-gulp.task('html', function() {
-  gulp.src('.src/html/**/*.html')
-    .pipe(gulp.dest('./dist'))
-});
-
-gulp.task('images', function(){
-  return gulp.src('./src/images/**/*.png')
-    .pipe(gulp.dest('./dist/images'))
-});
-
-gulp.task('js', function(){
-  return gulp.src('./src/js/**/*.js')
-    .pipe(gulp.dest('./dist/js'))
+  gulp.watch('./scss/**/*.scss', ['styles']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
 
-gulp.task('default', ['styles', 'serve', 'html', 'images', 'js']);
+gulp.task('default', ['styles', 'serve']);
