e42a70b2e910a601b877f573752c1939d4602c0d
Sturmol <sturmol@hotmail.com>
2016-10-10 16:29:52 +0200

    scrollspy added

--- js/scrolling.js
+++ js/scrolling.js
@@ -3,4 +3,10 @@ $(function () {
     $(window).on("load resize", function () {
         $(".fill-screen").css("height", window.innerHeight);
     });
+
+    //add bootstrap's scrollspy
+    $('body').scrollspy({
+    	target: '.navbar'
+    });
+    
 });
